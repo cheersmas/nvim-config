3be24cd22009b0b06dd1b6ffe889cf57029d3a24
cheersmas <shradhankr0028@gmail.com>
2023-04-03 20:37:41 +0530

    add foramtter

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -5,12 +5,15 @@ if not present then
 end
 
 local b = null_ls.builtins
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
 
 local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "javascript", "typescript" } }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
@@ -22,4 +25,28 @@ local sources = {
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.keymap.set("n", "<Leader>ft", function()
+        vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds { buffer = bufnr, group = group }
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr, async = async }
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method "textDocument/rangeFormatting" then
+      vim.keymap.set("x", "<Leader>ft", function()
+        vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
 }
